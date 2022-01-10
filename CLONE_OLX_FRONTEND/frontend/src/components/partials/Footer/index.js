a1d85794e9447ceb507505dae6ae2b84005f1c79
Vitor Geovani M. Silva <sincerev36@gmail.com>
2022-01-10 09:50:50 -0300

    Add files via upload

---
+++ CLONE_OLX_FRONTEND/frontend/src/components/partials/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FooterArea } from './styled';
+
+const Footer = () => {
+    return (
+        <FooterArea>
+            Todos os direitos reservados<br />
+            OLX Clone | AlunosB7Web - Vitor Geovani Melo Silva
+        </FooterArea>
+    );
+}
+
+export default Footer;
---
+++ CLONE_OLX_FRONTEND/frontend/src/helpers/OlxAPI.js
@@ -0,0 +1,168 @@
+import Cookies from 'js-cookie';
+import qs from 'qs';
+
+const BASEAPI = 'http://alunos.b7web.com.br:501';
+
+const apiFetchFile = async (endpoint, body) => {
+    if(!body.token) {
+        let token = Cookies.get('token');
+        if(token) {
+            body.append('token', token);
+        }
+    }
+    const res = await fetch(BASEAPI+endpoint, {
+        method:'POST',
+        body
+    });
+    const json = await res.json();
+
+    if(json.notallowed) {
+        window.location.href = '/signin';
+        return;
+    }
+
+    return json;
+};
+
+const apiFetchPost = async (endpoint, body) => {
+    if(!body.token) {
+        let token = Cookies.get('token');
+        if(token) {
+            body.token = token;
+        }
+    }
+    const res = await fetch(BASEAPI+endpoint, {
+        method:'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(body)
+    });
+    const json = await res.json();
+
+    if(json.notallowed) {
+        window.location.href = '/signin';
+        return;
+    }
+
+    return json;
+}
+
+async function apiFetchPut(endpoint, body) {
+    if (!body.token) {
+      let token = Cookies.get('token');
+      if (token) {
+        body.token = token;
+      }
+    }
+  
+    const res = await fetch(BASEAPI + endpoint, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await res.json()
+  
+    if (data.notallowed) {
+      window.location.href = '/signin';
+      return;
+    }
+    return data;
+}
+
+const apiFetchGet = async (endpoint, body = []) => {
+    if(!body.token) {
+        let token = Cookies.get('token');
+        if(token) {
+            body.token = token;
+        }
+    }
+    const res = await fetch(`${BASEAPI+endpoint}?${qs.stringify(body)}`);
+    const json = await res.json();
+
+    if(json.notallowed) {
+        window.location.href = '/signin';
+        return;
+    }
+
+    return json;
+}
+
+const OlxAPI = {
+
+    login:async (email, password) => {
+        const json = await apiFetchPost(
+            '/user/signin',
+            {email, password}
+        );
+        return json;
+    },
+
+    register:async (name, email, password, stateLoc) => {
+        const json = await apiFetchPost(
+            '/user/signup',
+            {name, email, password, state:stateLoc}
+        );
+        return json;
+    },
+
+    getStates:async () => {
+        const json = await apiFetchGet(
+            '/states'
+        );
+        return json.states;
+    },
+
+    getCategories:async () => {
+        const json = await apiFetchGet(
+            '/categories'
+        );
+        return json.categories;
+    },
+
+    getAds:async (options) => {
+        const json = await apiFetchGet(
+            '/ad/list',
+            options
+        );
+        return json;
+    },
+
+    getAd:async (id, other = false) => {
+        const json = await apiFetchGet(
+            '/ad/item',
+            {id, other}
+        );
+        return json;
+    },
+
+    addAd:async (fData) => {
+        const json = await apiFetchFile (
+            '/ad/add',
+            fData
+        );
+        return json;
+    },
+
+    getUser: async () => {
+        const response = await apiFetchGet(
+          '/user/me'
+        );
+        return response;
+    },
+    
+    updateUser: async (name, email, state, password) => {
+        const response = await apiFetchPut(
+          '/user/me',
+          { name, email, state, password }
+        );
+        return response;
+    }
+
+};
+
+export default () => OlxAPI;
