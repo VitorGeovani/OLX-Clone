c607305b141d8e922db6989578af302d64ad6bdd
Vitor Geovani M. Silva <sincerev36@gmail.com>
2022-01-10 09:55:20 -0300

    Add files via upload

---
+++ build/precache-manifest.c55ecb4c348b756f08b8e2e56af0a204.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c5290d498d95f603d66d89fecc575f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "defdf61d2acb3b5adaab",
+    "url": "/static/css/2.3b64944d.chunk.css"
+  },
+  {
+    "revision": "632c91afdd95529e0cd0",
+    "url": "/static/css/main.58bd613d.chunk.css"
+  },
+  {
+    "revision": "defdf61d2acb3b5adaab",
+    "url": "/static/js/2.3b237ce0.chunk.js"
+  },
+  {
+    "revision": "632c91afdd95529e0cd0",
+    "url": "/static/js/main.dbd27cf4.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
